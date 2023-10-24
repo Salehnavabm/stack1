834044745e83b8e5a5ecf41a2fe32c22e7f31d08
salehnavab <salehnavabm@gmail.com>
2023-10-24 20:26:41 +0330

    Add project files.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,62 @@
+﻿#include <iostream>
+#define MAX_SIZE 100
+
+using namespace std;
+
+class Stack {
+private:
+    int top;
+    int data[MAX_SIZE];
+public:
+    Stack() {
+        top = -1;
+    }
+
+    bool isEmpty() {
+        return (top == -1);
+    }
+
+    bool isFull() {
+        return (top == MAX_SIZE - 1);
+    }
+
+    void push(int value) {
+        if (isFull()) {
+            cout << "Error: pooreh!" << endl;
+            return;
+        }
+        data[++top] = value;
+    }
+
+    void pop() {
+        if (isEmpty()) {
+            cout << "Error: khalii!" << endl;
+            return;
+        }
+        top--;
+    }
+
+    int peek() {
+        if (isEmpty()) {
+            cout << "Error: khalii!" << endl;
+            return -1;
+        }
+        return data[top];
+    }
+};
+
+int main() {
+    Stack myStack;
+
+    myStack.push(5);
+    myStack.push(10);
+    myStack.push(15);
+
+    cout << "element: ";
+    while (!myStack.isEmpty()) {
+        cout << myStack.peek() << " ";
+        myStack.pop();
+    }
+
+    return 0;
+}
